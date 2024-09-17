142f6d168a66edb90307566a1cd625590dfde779
Java <73376510+Javascript-void0@users.noreply.github.com>
2024-09-17 00:06:20 -0400

    coc, utlisnips

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 REM symlinks for windows
 
-REM nvim
-mklink C:\Users\Java\AppData\Local\nvim\init.vim C:\Users\Java\Downloads\GitHub\dotfiles\nvim\init.vim
+REM nvim config directory
+mklink /D C:\Users\Java\AppData\Local\nvim\ C:\Users\Java\Downloads\GitHub\dotfiles\nvim\
+REM fastfetch config
 mklink C:\Users\Java\.config\fastfetch\config.jsonc C:\Users\Java\Downloads\GitHub\dotfiles\fastfetch\config.jsonc
