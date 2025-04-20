1af792d6d174859201d1939de945eeccb41dc56b
Java <73376510+Javascript-void0@users.noreply.github.com>
2025-04-20 15:56:38 -0400

    linux

---
+++ install2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# symlinks
+
+ln -s ~/Downloads/GitHub/dotfiles/rofi        ~/.config/rofi
+ln -s ~/Downloads/GitHub/dotfiles/qutebrowser ~/.config/qutebrowser
+ln -s ~/Downloads/GitHub/dotfiles/i3          ~/.config/i3
+ln -s ~/Downloads/GitHub/dotfiles/i3status    ~/.config/i3status
